2fe95149125caa15a3b852de91125d574a613ec1
axxdang <blue247114@gmail.com>
2024-05-13 00:47:21 +0700

    first commit

---
+++ src/main/java/swp391/SPS/SpsApplication.java
@@ -0,0 +1,13 @@
+package swp391.SPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpsApplication.class, args);
+	}
+
+}
