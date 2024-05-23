dc60505120fdfbe5dd6ba2e89b49e3c9be7b64e1
axxdang <blue247114@gmail.com>
2024-05-24 03:11:15 +0700

    config thymeleaf

--- src/main/java/swp391/SPS/controllers/MainController.java
+++ src/main/java/swp391/SPS/controllers/MainController.java
@@ -1,12 +1,20 @@
 package swp391.SPS.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import swp391.SPS.entities.User;
 
 @Controller
 public class MainController {
   @RequestMapping("/login")
   public String login() {
     return "login";
+  }
+
+  @PostMapping("/loginpost")
+  public String loginPost(@ModelAttribute User user) {
+    return "redirect:index";
   }
 }
