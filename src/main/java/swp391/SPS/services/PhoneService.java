a7e459defd743b05dd9d7c8cd3b705304cedeebc
longdt <blue247114@gmail.com>
2024-05-30 23:43:40 +0700

    FE mapping data

---
+++ src/main/java/swp391/SPS/services/PhoneService.java
@@ -0,0 +1,7 @@
+package swp391.SPS.services;
+
+import swp391.SPS.entities.Phone;
+
+public interface PhoneService {
+    void addPhone(Phone phone);
+}
---
+++ src/main/java/swp391/SPS/services/impls/PhoneServiceImpl.java
@@ -0,0 +1,19 @@
+package swp391.SPS.services.impls;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import swp391.SPS.entities.Phone;
+import swp391.SPS.repositories.PhoneRepository;
+import swp391.SPS.services.PhoneService;
+
+@Service
+public class PhoneServiceImpl implements PhoneService {
+
+    @Autowired
+    private PhoneRepository phoneRepository;
+
+    @Override
+    public void addPhone(Phone phone) {
+        phoneRepository.save(phone);
+    }
+}
