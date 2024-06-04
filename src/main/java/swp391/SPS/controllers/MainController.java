9019491f014ae54ee499629b295e01981ed324cf
namkhanhhhh <khanhnguyennam04@gmail.com>
2024-06-04 22:05:01 +0700

    update

--- src/main/java/swp391/SPS/controllers/MainController.java
+++ src/main/java/swp391/SPS/controllers/MainController.java
@@ -54,19 +54,6 @@ public class MainController {
         return "register";
     }
 
-    @GetMapping("/cart")
-    public String cart(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
-            model.addAttribute("isLogin", false);
-            return "login";
-        }
-        model.addAttribute("isLogin", true);
-        model.addAttribute("username", authentication.getName());
-
-        return "cart";
-    }
-
     @GetMapping("/checkout")
     public String checkout(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
