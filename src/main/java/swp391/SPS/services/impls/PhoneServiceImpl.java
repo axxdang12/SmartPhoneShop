a12243e14763d4d0068d9661e7c0d5af5d643d49
phanhien87 <phanminhhien782004@gmail.com>
2024-06-05 21:41:05 +0700

    update1

--- src/main/java/swp391/SPS/services/impls/PhoneServiceImpl.java
+++ src/main/java/swp391/SPS/services/impls/PhoneServiceImpl.java
@@ -1,10 +1,13 @@
 package swp391.SPS.services.impls;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import swp391.SPS.entities.Brand;
 import swp391.SPS.entities.Phone;
+import swp391.SPS.repositories.BrandRepository;
 import swp391.SPS.repositories.PhoneRepository;
 import swp391.SPS.services.PhoneService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,6 +15,8 @@ public class PhoneServiceImpl implements PhoneService {
 
     @Autowired
     private PhoneRepository phoneRepository;
+    @Autowired
+    private BrandRepository brandRepository;
 
 
     @Override
@@ -22,5 +27,21 @@ public class PhoneServiceImpl implements PhoneService {
     @Override
     public void addPhone(Phone phone) {
         phoneRepository.save(phone);
+    }
+
+    @Override
+    public Phone getPhoneByID(int id) {
+       Phone p =  phoneRepository.getReferenceById(id);
+       return p;
+    }
+
+    @Override
+    public List<Phone> getPhoneByBrand(int id) {
+        Brand brand = brandRepository.getReferenceById(id);
+        List<Phone> l = new ArrayList<Phone>();
+        for (int i = 0; i < findAllPhone().size(); i++) {
+            if(findAllPhone().get(i).getBrand().equals(brand)) l.add(findAllPhone().get(i));
+        }
+        return l;
     }
 }
