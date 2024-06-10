591937c819c20f2f00a743db4903d63ff22def1d
longdt <blue247114@gmail.com>
2024-06-10 16:48:00 +0700

    coding send email and validate BE for register controller

--- src/main/java/swp391/SPS/entities/EmailDetails.java
+++ src/main/java/swp391/SPS/entities/EmailDetails.java
@@ -1,12 +1,14 @@
 package swp391.SPS.entities;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class EmailDetails {
   private String recipient;
   private String msgBody;
