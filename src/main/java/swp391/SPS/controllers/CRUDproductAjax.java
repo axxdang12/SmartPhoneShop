0240594478e3600495580e9c3895715dd93c13a8
phanhien87 <phanminhhien782004@gmail.com>
2024-06-19 03:14:39 +0700

    CRUDphone,searchAllProduct

--- src/main/java/swp391/SPS/controllers/CRUDproductAjax.java
+++ src/main/java/swp391/SPS/controllers/CRUDproductAjax.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import swp391.SPS.entities.Phone;
 import swp391.SPS.services.BrandService;
 import swp391.SPS.services.PhoneService;
+import swp391.SPS.services.PictureService;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,6 +19,28 @@ public class CRUDproductAjax {
 
     @Autowired
     BrandService brandService;
+
+    @Autowired
+    PictureService pictureService;
+
+    @PostMapping("/api/add-product")
+    public ResponseEntity<Map<String, Object>> addPhone( @RequestBody Phone phone) {
+        Phone p = Phone.builder().productName(phone.getProductName()).phoneId(phone.getPhoneId())
+                .cpu(phone.getCpu()).ram(phone.getRam()).sim(phone.getSim()).price(phone.getPrice()).
+                camera(phone.getCamera()).memory(phone.getMemory()).origin(phone.getOrigin()).
+                brand(phone.getBrand()).picture(phone.getPicture()).releaseDate(phone.getReleaseDate())
+                .display(phone.getDisplay()).build();
+
+        if (p != null) {
+            Map<String, Object> response = new HashMap<>();
+            response.put("listBrand", brandService.findAllBrand());
+            response.put("phone", phone);
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 
     @PostMapping("/api/edit-product")
     public ResponseEntity<Map<String, Object>> getPhoneById( @RequestBody Map<String, Integer> request) {
@@ -38,6 +61,15 @@ public class CRUDproductAjax {
         model.addAttribute("listBrand", brandService.findAllBrand());
         phoneService.editPhone(phone);
         return ResponseEntity.ok("Product has id is "+ phone.getPhoneId() +"updated sucessfully");
+    }
+
+    @PostMapping("/api/delete-product")
+    public ResponseEntity<String> deletePhone( @RequestBody Map<String, Integer> request){
+        int phoneid = request.get("id");
+        Phone phone = phoneService.getPhoneByID(phoneid);
+        pictureService.deletePicture(phone.getPicture());
+        phoneService.deletephone(phone);
+        return ResponseEntity.ok("Delete successfully");
     }
 
 
